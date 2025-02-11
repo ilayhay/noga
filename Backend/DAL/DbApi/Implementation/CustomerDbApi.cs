6739012116605896d716a1d37a4fde48adc50c85
ilay <ilayhay10@gmail.com>
2025-02-11 15:45:41 -0800

    adding as no tracking at findById

--- Backend/DAL/DbApi/Implementation/CustomerDbApi.cs
+++ Backend/DAL/DbApi/Implementation/CustomerDbApi.cs
@@ -11,12 +11,12 @@ public class CustomerDbApi : ICustomerDbApi
 
     public async Task<IEnumerable<Customer>> GetAllAsync()
     {
-        return await _context.Customers.AsNoTracking().ToListAsync();
+        return await _context.Customers.AsNoTracking().Where(customer => !customer.IsDeleted).ToListAsync();
     }
 
     public async Task<Customer?> GetByIdAsync(int id)
     {
-        return await _context.Customers.FindAsync(id);
+        return await _context.Customers.AsNoTracking().FirstOrDefaultAsync(customer => customer.Id == id);
     }
 
     public async Task<Customer> AddAsync(Customer customer)
